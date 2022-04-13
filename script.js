59dfc3bef5efdcd0b2bfe15322f7161624e50e62
Matheus Tostes <matheustostes@hotmail.com>
2022-04-13 15:39:07 -0300

    Extract teams and infos on objects

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const sendTeams = document.getElementById("send-teams");
+const teamsBlock = document.getElementById("teams-block");
 
 const getTeams = () => {
   let teams = document.getElementById("teams-input").value;
+  teams = teams.trim();
   return teams;
 };
 
 const logTeams = () => {
+  participants = {};
   teams = getTeams();
-  console.log(teams);
+  teamsBlock.innerText = teams;
+
+  const splited = teams.split(/\n/);
+
+  for (let string of splited) {
+    const list = string.split(";");
+    participants[list[0]] = {
+      house: list[1],
+    };
+  }
 };
 
 sendTeams.addEventListener("click", logTeams);
